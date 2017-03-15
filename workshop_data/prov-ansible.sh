2158f2aca4aa19948ebb2c0b9a45ea13902f6756
Diego Morales <morales@propus.com.br>
2017-03-15 19:54:49 -0300

    Fixes after second workshop at stone

--- workshop_data/prov-ansible.sh
+++ workshop_data/prov-ansible.sh
@@ -3,8 +3,8 @@
 # Populate /etc/hosts
 cat >> /etc/hosts <<EOF
 192.168.100.11 m1.local m1
-192.168.100.12 m1.local m2
-192.168.100.13 m1.local m3
+192.168.100.12 m2.local m2
+192.168.100.13 m3.local m3
 EOF
 
 # Install EPEL
